fcd7b3094f2d526c259a2b7e9a297c9ed6711581
Herz Andreas <andreas.herz@fill.co.at>
2022-03-13 17:08:14 +0100

    #4 - UI

--- src/app/modules/bitcoin-details/bitcoin-details.component.ts
+++ src/app/modules/bitcoin-details/bitcoin-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { filter } from 'rxjs';
+import { BlockchainDetails } from 'src/app/shared/models/blockchain.models';
+
 import { BcDetailsApiService } from './../../shared/services/api/bc-details-api.service';
 
 @Component({
@@ -8,12 +11,16 @@ import { BcDetailsApiService } from './../../shared/services/api/bc-details-api.
   styleUrls: ['./bitcoin-details.component.scss']
 })
 export class BitcoinDetailsComponent implements OnInit {
+  public details?: BlockchainDetails;
+
   constructor(private bcDetailsApi: BcDetailsApiService) {
     this.bcDetailsApi.updateBlockchainInfo();
   }
 
   ngOnInit() {
-    this.bcDetailsApi.currentBlockchainInfo$.subscribe(el => console.log(el));
+    this.bcDetailsApi.currentBlockchainInfo$
+      .pipe(filter<BlockchainDetails | undefined>(Boolean))
+      .subscribe((details: BlockchainDetails) => (this.details = details));
   }
 
   public refresh(): void {
