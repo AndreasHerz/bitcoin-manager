488cd6e3dd1123ebe08de79686495bedea9854ce
Herz Andreas <andreas.herz@fill.co.at>
2022-03-13 18:32:57 +0100

    #4 - Refactoring

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,16 @@
 export const environment = {
   production: true,
-  dashboard: {
-    blockchain_ticker: 'https://blockchain.info/ticker'
-  },
-  details: {
-    marketcup: 'https://blockchain.info/q/marketcap',
-    totalbc: 'https://blockchain.info/q/totalbc',
-    '24hrtransactioncount': 'https://blockchain.info/q/24hrtransactioncount',
-    '24hrbtcsent': 'https://blockchain.info/q/24hrbtcsent',
-    hashrate: 'https://blockchain.info/q/hashrate',
-    getdifficulty: 'https://blockchain.info/q/getdifficulty'
+  api: {
+    dashboard: {
+      blockchain_ticker: 'https://blockchain.info/ticker'
+    },
+    details: {
+      marketcup: 'https://blockchain.info/q/marketcap',
+      totalbc: 'https://blockchain.info/q/totalbc',
+      '24hrtransactioncount': 'https://blockchain.info/q/24hrtransactioncount',
+      '24hrbtcsent': 'https://blockchain.info/q/24hrbtcsent',
+      hashrate: 'https://blockchain.info/q/hashrate',
+      getdifficulty: 'https://blockchain.info/q/getdifficulty'
+    }
   }
 };
